9fbecc5680111a697e25300b59d0b4a95a8b563d
fuqinqin <904571943@qq.com>
2018-04-15 23:56:52 +0800

    spring-jms 未完待续

---
+++ web-app/src/main/java/org/fuqinqin/code/lms/normal/AppProducer.java
@@ -0,0 +1,21 @@
+package org.fuqinqin.code.lms.normal;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @desc ActiveMQ生产者
+ * */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {""})
+public class AppProducer {
+
+    public static void main(String[] args) {
+
+        //
+
+    }
+
+}
